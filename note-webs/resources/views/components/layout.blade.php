64d566c38c3c4ec61a33ab1426294c8fb7d7c5ba
jgeb28 <jan-geb28@wp.pl>
2024-11-07 23:15:02 +0100

    clonning fix

--- note-webs/resources/views/components/layout.blade.php
+++ note-webs/resources/views/components/layout.blade.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Note App</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <x-navbar />
